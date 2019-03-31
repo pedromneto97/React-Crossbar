f0d7e4aae0cb27b8cb4b290362dedc4a3e93e359
Pedro Neto <pedromneto97@gmail.com>
2019-03-31 11:56:31 -0300

    Layout bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Col, Container, Row} from 'react-bootstrap';
+import './assets/css/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './views/Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Container fluid={true}>
+        <Row>
+            <Col>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact={true} component={Home}/>
+                    </Switch>
+                </BrowserRouter>
+            </Col>
+        </Row>
+    </Container>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
